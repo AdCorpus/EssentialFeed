7f6dac415e02f60b451bf7f8f57ed22639c34fdd
AdPiscinam <lab@walimaloui.com>
2023-01-30 15:07:44 +0100

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  EssentialFeed
+//
+//  Created by Walim Aloui on 30/01/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
